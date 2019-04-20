139dc3c01f552093b7451a69a8ed2706bbec84d9
jamalav <jamalav14@gmail.com>
2019-04-20 18:47:10 +0500

    lab6(update)

--- webpage/index.php
+++ webpage/index.php
@@ -3,9 +3,37 @@
 	<head>
 		<title>Validating Forms</title>
 		<link href="style.css" type="text/css" rel="stylesheet" />
+		<style media="screen">
+			.error {
+				color: blue;
+			}
+		</style>
 	</head>
 	
 	<body>
+	<?php
+		error_reporting(0);
+		$name=$_REQUEST["name"];
+		$email=$_REQUEST["email"];
+		$username=$_REQUEST["username"];
+		$password=$_REQUEST["password"];
+		$confirmedpassword=$_REQUEST["confirmedpassword"];
+		$dateOfBirth=$_REQUEST["dateOfBirth"];
+		$gender= isset($_REQUEST["gender"])? $_REQUEST["maritalStatut"] : null;
+		$maritalStatus=$_REQUEST["maritalStatus"];
+		$address=$_REQUEST["address"];
+		$city=$_REQUEST["city"];
+		$postalCode=$_REQUEST["postalCode"];
+		$homePhone=$_REQUEST["homePhone"];
+		$mobilePhone=$_REQUEST["mobilePhone"];
+		$cardNumber=$_REQUEST["cardNumber"];
+		$cardExpiryDate=$_REQUEST["cardExpiryDate"];
+		$salary=$_REQUEST["salary"];
+		$website=$REQUEST["website"];
+		
+	?>
+		
+		
 		<h1>Registration Form</h1>
 
 		<p>
